28be9bf148ea6bd276cdd9c8eb0cc29d3be26359
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-08-11 14:55:24 +0300

    add

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
-const baseUrl =
-	process.env.NODE_ENV === "production"
-		? "https://academy-project-nu.vercel.app"
-		: "http://localhost:3000";
+const baseUrl = "https://academy-project-nu.vercel.app"
+	// process.env.NODE_ENV === "production"
+	// 	? "https://academy-project-nu.vercel.app"
+	// 	: "http://localhost:3000";
 
 export default baseUrl;
