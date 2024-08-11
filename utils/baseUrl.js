8cdae9871c9f3b6aaea2e0777c5dfb6886874b31
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-08-11 14:46:18 +0300

    add

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
 	process.env.NODE_ENV === "production"
-		? "https://lisbo-react.envytheme.com"
+		? "https://academy-project-nu.vercel.app"
 		: "http://localhost:3000";
 
 export default baseUrl;
